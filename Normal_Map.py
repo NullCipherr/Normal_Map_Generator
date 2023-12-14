3d1ac2792f9e175c4114546cf5a057330780098c
NullCipherr <blendsteal@gmail.com>
2023-12-14 14:53:44 -0300

    Update Ambient Occlusion

--- Normal_Map.py
+++ Normal_Map.py
@@ -31,7 +31,7 @@ input_image_path = os.path.join(input_folder, input_image)
 # Parameters
 #   - input_path: Path where the desired image is located.
 ##############################################################################
-def load_image(input_path):
+def load_image(input_path) :
     try:
         image = cv2.imread(input_path)
 
@@ -53,9 +53,11 @@ def load_image(input_path):
 #   - output_name: Output name for the saved image.
 #   - output_folder: Folder where the image will be saved.
 ##############################################################################
-def save_image(image, output_name, output_folder):
+def save_image(image, output_name, output_folder, resolution = (2048, 2048)) :
     
-    print("\nOutput name -> ", output_name, "\nOutput folder -> ", output_folder)
+    image = cv2.resize(image, resolution)
+    
+    print("\nOutput name -> ", output_name, "\nOutput folder -> ", output_folder, "\nResolution -> ", resolution)
     
     # Create the complete path to the location
     output_path = os.path.join(output_folder, output_name)
@@ -73,7 +75,7 @@ def save_image(image, output_name, output_folder):
 # Parameters
 #   - image: Desired image to calculate the normal map.
 ##############################################################################
-def calculate_normal_map(image):
+def generate_normal_map(image):
     # Convert the image to grayscale (necessary for gradient calculation)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -113,6 +115,31 @@ def calculate_normal_map(image):
     return (normal_map * 128 + 128).astype(np.uint8)
 
 
+def generate_ambient_occlusion(image, radius=1, strength=0.9, gamma=1.0, exposure=1.0):
+    # Converte a imagem para escala de cinza diretamente
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calcula o mapa de oclusão usando um filtro de desfoque radial
+    occlusion_map = cv2.GaussianBlur(gray_image.astype(np.float32), (0, 0), radius)
+
+    # Normaliza o mapa de oclusão para o intervalo [0, 1]
+    occlusion_map = cv2.normalize(occlusion_map, None, 0, 1, cv2.NORM_MINMAX)
+
+    # Ajusta a intensidade da oclusão
+    occlusion_map = 1 - occlusion_map * strength
+
+    # Ajusta o contraste do mapa de oclusão usando a correção gama
+    occlusion_map = np.power(occlusion_map, gamma)
+
+    # Ajusta a exposição do mapa de oclusão
+    occlusion_map = np.clip(occlusion_map * exposure, 0, 1)
+
+    # Converte o mapa de oclusão para valores de 8 bits (0-255)
+    occlusion_map = (occlusion_map * 255).astype(np.uint8)
+
+    return occlusion_map
+
+
 if __name__ == "__main__" :
 
     # Load the input image.
@@ -120,7 +147,9 @@ if __name__ == "__main__" :
 
     if input_image is not None:
         # Apply the 'calculate_normal_map' function to the input image.
-        normalMap = calculate_normal_map(input_image)
+        normal_map = generate_normal_map(input_image)
+        ambient_occlusion = generate_ambient_occlusion(input_image)
 
         # Save the input image.
-        save_image(normalMap, "Normal_Map.jpg", output_folder)
+        save_image(normal_map, "NM.jpg", output_folder)
+        save_image(ambient_occlusion, "AO.jpg", output_folder)
